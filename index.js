c26b21c7b1d6f7c87a46f122117b1fd0aad588f5
zurupe <pablozurita.jr@outlook.com>
2026-01-27 12:23:23 -0500

    Corrección de comillas dobles a simples ene l index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express"); //Pablo Zurita
+const express = require('express'); //Pablo Zurita
 const app = express();
 const PORT = 3000;
 
 app.get('/', (_req, res) => {
-    res.send("Integración continua funcionando!");
+    res.send('Integración continua funcionando!');
 });
 
 app.listen(PORT, () => {
