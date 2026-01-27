5938a833530603fc0f93e55371f1ad177231d703
zurupe <pablozurita.jr@outlook.com>
2026-01-27 12:20:11 -0500

    Correción de semicolons en index

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = 3000;
 
 app.get('/', (_req, res) => {
     res.send("Integración continua funcionando!")
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor esta ejecutandose en el puerto ${PORT}`)
-})
+});
