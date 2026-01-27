333a08f52807b7f62aa4643ef49e614d192fd5d3
zurupe <pablozurita.jr@outlook.com>
2026-01-27 12:25:44 -0500

    comit prueba

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (_req, res) => {
-    res.send('Integración continua funcionando!');
+    res.send('Integración continua funcionando!')
 });
 
 app.listen(PORT, () => {
