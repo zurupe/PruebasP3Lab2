fa8634ceb8421f38802c245b6eaa9b1cb9e12aea
zurupe <pablozurita.jr@outlook.com>
2026-01-27 12:33:52 -0500

    Prueba de divición arreglada

--- div.test.js
+++ div.test.js
@@ -1,5 +1,5 @@
 const div = require('./div.js');
 
-test('Division para 0 debe ser 7', () => {
-    expect(div(3, 0)).toBe(7);
+test('Division entre 15 y 5 debe ser 3', () => {
+    expect(div(15, 5)).toBe(3);
 });
